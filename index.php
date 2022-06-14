606c7febbb19ecd98d044f9c88f72796103f7846
Rodrigo <gatopunk99@gmail.com>
2022-06-14 03:46:50 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+        if ($_POST){
+            $variable = str_replace(' ', '', trim($_POST['input'])) ;
+            if ((preg_match('/(\d+)(?:\s*)([\+\-\*\/])(?:\s*)(\d+)/', $variable, $matches) !== FALSE)){
+                $operador = $matches[2];
+                switch ($operador){
+                    case '+':
+                        $p = $matches[1] + $matches[3];
+                        break;
+                    case '-':
+                        $p = $matches[1] - $matches[3];
+                        break;
+                    case '*':
+                        $p = $matches[1] * $matches[3];
+                        break;
+                    case '/':
+                        $p = $matches[1] / $matches[3];
+                        break;
+                    echo $p;
+                }
+            }
+            
+    }
+    ?>
+
+    <form action="index.php" method="POST">
+        <input name="input" type="text">
+    </form>
+
+</body>
+</html>
